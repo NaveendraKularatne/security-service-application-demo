12adcc9c21d83fb9aed2dc2db5454012837338f6
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-09-22 01:34:30 +0530

    Initial commit

---
+++ src/test/java/com/kularatne/userservice/UserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kularatne.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
