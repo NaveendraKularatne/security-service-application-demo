3b6bc088a43f8dd66518762ce51f1058cc47f2e6
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-09-22 23:49:39 +0530

    Update User Service Implementation - in service

--- src/main/java/com/kularatne/userservice/service/UserServiceImpl.java
+++ src/main/java/com/kularatne/userservice/service/UserServiceImpl.java
@@ -8,9 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @Service
@@ -23,16 +20,19 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User saveUser(User user) {
+        log.info("Saving new user {} to the database", user.getName());
         return userRepo.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
+        log.info("Saving new role {} to the database", role.getName());
         return roleRepo.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role {} to user {}", roleName, username);
         User foundUser = userRepo.findByUsername(username);
         Role foundRoles = roleRepo.findByName(roleName);
 
@@ -44,11 +44,13 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User getUser(String username) {
+        log.info("Fetching user {} ", username);
         return userRepo.findByUsername(username);
     }
 
     @Override
     public List<User> getUsers() {
+        log.info("Fetching all users");
         return userRepo.findAll();
     }
 }
