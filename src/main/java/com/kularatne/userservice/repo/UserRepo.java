97284a9f5da8200d34048a3790f8be8fbdffe907
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-09-23 14:04:46 +0530

    Update UserRepo class

--- src/main/java/com/kularatne/userservice/repo/UserRepo.java
+++ src/main/java/com/kularatne/userservice/repo/UserRepo.java
@@ -2,7 +2,9 @@ package com.kularatne.userservice.repo;
 
 import com.kularatne.userservice.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
