a4eddf626118ed35afbbbb2ff63d0fe404c8b87f
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-09-23 23:51:05 +0530

    Save users and roles to DB and get data to client from DB

--- src/main/java/com/kularatne/userservice/UserserviceApplication.java
+++ src/main/java/com/kularatne/userservice/UserserviceApplication.java
@@ -1,7 +1,14 @@
 package com.kularatne.userservice;
 
+import com.kularatne.userservice.domain.Role;
+import com.kularatne.userservice.domain.User;
+import com.kularatne.userservice.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class UserserviceApplication {
@@ -10,4 +17,26 @@ public class UserserviceApplication {
 		SpringApplication.run(UserserviceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run (UserService userService){
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(null,"John Travolta", "john", "john", new ArrayList<>()));
+			userService.saveUser(new User(null,"Will Smith", "will", "will", new ArrayList<>()));
+			userService.saveUser(new User(null,"Jim Carry", "jim", "jim", new ArrayList<>()));
+			userService.saveUser(new User(null,"Tom Cruise", "tom", "tom", new ArrayList<>()));
+
+			userService.addRoleToUser("john", "ROLE_USER");
+			userService.addRoleToUser("john", "ROLE_MANAGER");
+			userService.addRoleToUser("will", "ROLE_MANAGER");
+			userService.addRoleToUser("jim", "ROLE_ADMIN");
+			userService.addRoleToUser("tom", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("tom", "ROLE_ADMIN");
+			userService.addRoleToUser("tom", "ROLE_USER");
+		};
+	}
 }
